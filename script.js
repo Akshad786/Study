e2c9e6c77babd007ae35b55fec868e07a61beb1c
Akshad786 <akshadshaikh84@gmail.com>
2026-09-08 20:44:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} 
+from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+
+
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 // Dark / Light Mode
 
 const themeBtn = document.getElementById("themeBtn");
